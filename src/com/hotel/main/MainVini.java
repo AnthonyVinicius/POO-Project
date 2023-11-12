9876ffd1adb28792d20bfba7690011eabe699c09
vjarruda <arrudavini25@gmail.com>
2023-11-12 20:55:02 -0300

    atualiza mainVini (incompleto)

--- src/com/hotel/main/MainVini.java
+++ src/com/hotel/main/MainVini.java
@@ -8,6 +8,7 @@ import com.hotel.pessoa.*;
 public class MainVini {
 	public static void main(String[] args) {
 		ArrayList<Quarto> quartosReserva = new ArrayList<Quarto>();
+		ArrayList<Reserva> reservas = new ArrayList<Reserva>();
 		
 		Telefone telefone = new Telefone("81", "99876-4567");
 		Endereco endereco = new Endereco("Palmares", "Nova Palmares", "Rua A", "7");
@@ -21,7 +22,27 @@ public class MainVini {
 		
 		Cama cama = new Cama("Simples");
 		Quarto quarto = new Quarto(1, cama);
+		quartosReserva.add(quarto);
+		
+		PagamentoCartao pagCart = new PagamentoCartao();
+		pagCart.setDataPagamento("12/11/2023");
+		pagCart.setMetodoPagamento("Débito");
+		pagCart.setNumero("5345 4353 3453 6757");
+		pagCart.setTipoCartao("MasterCard");
+		pagCart.setValorBase(100);
+		
+		Reserva reserva = new Reserva();
+		reserva.setCliente(cliente);
+		reserva.setFuncionario(funcionario);
+		reserva.setListaQuartos(quartosReserva);
+		reserva.setPagamento(pagCart);
+		reservas.add(reserva);
+		
 		Hotel hotel = new Hotel();
+		hotel.setNome("Hotel Estrelas");
+		hotel.setListaReservas(reservas);
+		
+		//incompleto(lembrar de serviços)
 		
 	}
 }
