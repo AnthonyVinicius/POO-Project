c715f0355f4703986bcb1befcb5bd12a6d057605
LuizBuarqueDev <luizgabrielbuarque>
2023-11-12 19:55:19 -0300

    Implemetação main (incompleto)

--- src/com/hotel/main/MainLuiz.java
+++ src/com/hotel/main/MainLuiz.java
@@ -2,6 +2,7 @@ package com.hotel.main;
 
 import java.util.ArrayList;
 
+import com.hotel.sistema.Cama;
 import com.hotel.sistema.Endereco;
 import com.hotel.sistema.Hotel;
 import com.hotel.sistema.Quarto;
@@ -10,13 +11,21 @@ import com.hotel.sistema.Reserva;
 public class MainLuiz {
 	public static void main(String[] args) {
 		
-		ArrayList<Quarto> listaQuartosR1 = new ArrayList<Quarto>();
+		System.out.println("Main Luiz");
 		
-		Reserva reserva1 = new Reserva("12/11/2023",Quarto);
+		ArrayList<Quarto> listaQuartosR1 = new ArrayList<Quarto>();
+		listaQuartosR1.add(new Quarto(11, new Cama("Simples")));
+		
+		ArrayList<Quarto> listaQuartosR2 = new ArrayList<Quarto>();
+		listaQuartosR2.add(new Quarto(12, new Cama("Dupla")));
+		
+		Reserva reserva1 = new Reserva();
 		
 		Endereco enderecoHotel = new Endereco("Palmares", "Cohab 1", "Rua Z", "255");
+		
 		Hotel hotel = new Hotel("Palace Hotel", enderecoHotel, null, null);
 		
+		listaQuartosR1.get(0).getCama();
 	}
 
 }
--- src/com/hotel/sistema/PagamentoDinheiro.java
+++ src/com/hotel/sistema/PagamentoDinheiro.java
@@ -1,11 +1,12 @@
-package com.hotel.sistema;
-
-public class PagamentoDinheiro extends Pagamento {
-
-	// TODO: mplementar uma forma de pegar o valor base da classe Pagamento
-	@Override
-	public void calcularPagamento() {
-		// TODO Auto-generated method stub
-
+	package com.hotel.sistema;
+	
+	public class PagamentoDinheiro extends Pagamento {
+	
+		// TODO: mplementar uma forma de pegar o valor base da classe Pagamento
+		@Override
+		public void calcularPagamento() {
+			// TODO Auto-generated method stub
+	
+		}
+	
 	}
-}
