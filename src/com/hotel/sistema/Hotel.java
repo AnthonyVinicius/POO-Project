8c9ce4fa7d4a73ef2143e2afeb0c617bfadbff2d
vjarruda <arrudavini25@gmail.com>
2023-11-17 14:32:31 -0300

    mudanças na classe hotel

--- src/com/hotel/sistema/Hotel.java
+++ src/com/hotel/sistema/Hotel.java
@@ -2,22 +2,28 @@ package com.hotel.sistema;
 
 import java.util.List;
 
+import com.hotel.pessoa.Gerente;
+
 public class Hotel {
 
 	private String nome;
 	private Endereco endereco;
+	private Telefone telefone;
+	private Gerente gerente;
 	private List<Reserva> listaReservas;
 
 	public Hotel() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Hotel(String nome, Endereco endereco, List<Reserva> listaReservas) {
+	public Hotel(String nome, Endereco endereco, List<Reserva> listaReservas, Telefone telefone, Gerente gerente) {
 		super();
 		this.nome = nome;
 		this.endereco = endereco;
 		this.listaReservas = listaReservas;
-	}
+		this.telefone = telefone;	
+		this.gerente = gerente;
+		}
 
 	public String getNome() {
 		return nome;
@@ -42,4 +48,22 @@ public class Hotel {
 	public void setListaReservas(List<Reserva> listaReservas) {
 		this.listaReservas = listaReservas;
 	}
+
+	public Telefone getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+
+	public Gerente getGerente() {
+		return gerente;
+	}
+
+	public void setGerente(Gerente gerente) {
+		this.gerente = gerente;
+	}
+	
+	
 }
