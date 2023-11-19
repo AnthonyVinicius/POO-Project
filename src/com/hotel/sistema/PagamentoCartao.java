24550694922ecb188ab7a4008550cadde0a28283
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2023-11-19 12:57:41 -0300

    Mudaça na estetica da classe PagamentoCartao

--- src/com/hotel/sistema/PagamentoCartao.java
+++ src/com/hotel/sistema/PagamentoCartao.java
@@ -8,7 +8,7 @@ public class PagamentoCartao extends Pagamento {
 
 	private String tipoCartao;
 	private String numero;
-	
+
 	private double valorFinal;
 
 	public PagamentoCartao() {
@@ -34,20 +34,23 @@ public class PagamentoCartao extends Pagamento {
 				System.out.println("---------------------------------------------------");
 				System.out.println("Cliente: " + reserva.getCliente().getNome());
 				System.out.println("Cpf: " + reserva.getCliente().getCpf());
+				System.out.println("Numero do cartão:" + getNumero());
+				System.out.println("Tipo Cartão: " + getMetodoPagamento());
 
 				for (Quarto quarto : reserva.getListaQuartos()) {
-					System.out.println("Quarto : " + valorQuarto);
+					System.out.printf("Quarto:---------R$%.2f\n", valorQuarto);
 					somaTotal += valorQuarto;
 
 					for (Cama cama : quarto.getCama()) {
-						System.out.println("Cama : " + valorCama);
+						System.out.printf("Cama:------------R$%.2f\n", valorCama);
 						somaTotal += valorCama;
 
 					}
 				}
 				valorFinal = somaTotal - somaTotal * 0.03;
-				System.out.println("Total: " + valorFinal);
-				System.out.println("--------------------------------------------------------------");
+				System.out.printf("\nValor Bruto:-----R$%.2f", somaTotal);
+				System.out.printf("\nValor Final:-----R$%.2f", valorFinal);
+				System.out.println("\n--------------------------------------------------------------");
 			}
 
 		} else if (tipoCartao.equalsIgnoreCase("Credito")) {
@@ -56,20 +59,24 @@ public class PagamentoCartao extends Pagamento {
 				System.out.println("---------------------------------------------------");
 				System.out.println("Cliente: " + reserva.getCliente().getNome());
 				System.out.println("Cpf: " + reserva.getCliente().getCpf());
+				System.out.println("Numero do cartão:" + getNumero());
+				System.out.println("Tipo Cartão: " + getMetodoPagamento());
 
 				for (Quarto quarto : reserva.getListaQuartos()) {
-					System.out.println("Quarto : " + valorQuarto);
+					System.out.printf("Quarto:---------R$%.2f\n", valorQuarto);
 					somaTotal += valorQuarto;
 
 					for (Cama cama : quarto.getCama()) {
-						System.out.println("Cama : " + valorCama);
+						System.out.printf("Cama:------------R$%.2f\n", valorCama);
 						somaTotal += valorCama;
 
 					}
 				}
 				valorFinal = somaTotal + somaTotal * 0.05;
-				System.out.println("Total: " + valorFinal);
-				System.out.println("--------------------------------------------------------------");
+				System.out.printf("\nValor Bruto:-----R$%.2f", somaTotal);
+				System.out.printf("\nValor Final:-----R$%.2f", valorFinal);
+				System.out.println("\n--------------------------------------------------------------");
+
 			}
 		} else {
 			throw new InvalidParameterException("Tipo de Cartão invalido , tente novamente");
